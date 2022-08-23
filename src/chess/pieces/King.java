7bab03a75b9fa8e85fc240cbce6927a592f89ef5
Marcus Cezar <marcusferraz98@gmail.com>
2022-08-23 17:23:00 -0300

    Implemented possible moves system of pieces in king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
@@ -21,6 +22,54 @@ public class King extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] matPossibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position auxPosition = new Position(0, 0);
+
+        auxPosition.setValues(position.getRow() - 1, position.getColumn());
+        if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+            matPossibleMoves[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        auxPosition.setValues(position.getRow() - 1, position.getColumn() + 1);
+        if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+            matPossibleMoves[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        auxPosition.setValues(position.getRow(), position.getColumn() + 1);
+        if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+            matPossibleMoves[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        auxPosition.setValues(position.getRow() + 1, position.getColumn() + 1);
+        if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+            matPossibleMoves[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        auxPosition.setValues(position.getRow() + 1, position.getColumn());
+        if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+            matPossibleMoves[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        auxPosition.setValues(position.getRow() + 1 , position.getColumn() - 1);
+        if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+            matPossibleMoves[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        auxPosition.setValues(position.getRow() , position.getColumn() - 1);
+        if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+            matPossibleMoves[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        auxPosition.setValues(position.getRow() - 1 , position.getColumn() - 1);
+        if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+            matPossibleMoves[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
         return matPossibleMoves;
+    }
+
+    private boolean canMove (Position position) {
+        ChessPiece auxPiece = (ChessPiece) getBoard().piece(position);
+        return auxPiece == null || auxPiece.getColor() != getColor();
     }
 }
